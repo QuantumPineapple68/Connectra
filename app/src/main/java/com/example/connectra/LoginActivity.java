a7fa4c48287d14d2ab78e2c6949c3a973563ec5d
QuantumPineapple <nachiketj14@gmail.com>
2024-11-01 15:43:56 +0530

    Auth Prep

--- app/src/main/java/com/example/connectra/LoginActivity.java
+++ app/src/main/java/com/example/connectra/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.example.connectra;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -9,7 +11,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     Button register;
     Button login;
@@ -18,13 +20,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        register=findViewById(R.id.register);
-        login=findViewById(R.id.login);
+        register=findViewById(R.id.button2);
+//        login=findViewById(R.id.login);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this , RegisterActivity.class));
+                finish();
+            }
+        });
     }
 }
